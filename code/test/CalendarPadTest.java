e081be3d1c24a08a693f7dfb4d964ecd03a88c6f
DESKTOP-M6Q2UFK\asus <1048904006@qq.com>
2019-05-12 17:42:01 +0800

    更改结构

--- code/test/CalendarPadTest.java
+++ code/test/CalendarPadTest.java
@@ -2,8 +2,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 import java.awt.*;
+
+import static org.junit.Assert.*;
 
 public class CalendarPadTest {
 
@@ -20,6 +21,7 @@ public class CalendarPadTest {
 
     @Test
     public void set_calendar() {
+
         //大月31天测试
         calendarPad.set_calendar(2019,5);
         assertEquals(31,calendarPad.daynum);
@@ -41,6 +43,14 @@ public class CalendarPadTest {
         calendarPad.set_calendar(2000,2);
         assertEquals(29,calendarPad.daynum);
 
+        //年份出范围测试
+        calendarPad.set_calendar(4500,5);
+        assertEquals(31,calendarPad.daynum);
+        calendarPad.set_calendar(4500,4);
+        assertEquals(30,calendarPad.daynum);
+        calendarPad.set_calendar(4500,2);
+        assertEquals(28,calendarPad.daynum);
+
 
 
     }
@@ -51,7 +61,10 @@ public class CalendarPadTest {
         assertEquals(Color.blue,calendarPad.showDay[3].getBackground()); //5月1日为目标日 为蓝色背景
         assertEquals(Color.white,calendarPad.showDay[4].getBackground()); //5月2日不是目标日 为白色背景
         assertEquals(Color.red,calendarPad.showDay[6].getForeground()); //周六红色字体
-        assertEquals(Color.red,calendarPad.showDay[7].getForeground()); //周日红色字体
+        assertEquals(Color.red,calendarPad.showDay[7].getForeground()); //周日红色字体\
+        assertEquals("",calendarPad.showDay[2].getText());
+
+
     }
 
 
--- code/test/NotePadTest.java
+++ code/test/NotePadTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import static org.junit.Assert.*;
+
 public class NotePadTest {
 
     private CalendarPad calendarPad;
